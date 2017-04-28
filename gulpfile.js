619de7ed32ac5fb3e67f0b110ef0737cbedf1283
nfxpnk <nfxpnk@gmail.com>
2017-04-28 15:22:22 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	scsslint = require('gulp-scss-lint'),
+	livereload = require('gulp-livereload');
+
+gulp.task('sass', function () {
+	return gulp.src('scss/styles.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('/css'))
+		.pipe(livereload());
+});
+
+gulp.task('scss-lint', function() {
+	return gulp.src('scss/styles.scss')
+		.pipe(scsslint());
+});
+
+gulp.task('sass:watch', function () {
+	livereload.listen();
+	gulp.watch(['scss/styles.scss'], ['sass', 'scss-lint']);
+	gulp.watch('index.html', livereload.reload);
+});
