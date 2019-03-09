c53b30b52abe7b8592c606bd6a09c07723db7172
nfxpnk <nfxpnk@gmail.com>
2019-03-10 00:21:21 +0200

    gulpfile fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	scsslint = require('gulp-scss-lint'),
 	livereload = require('gulp-livereload');
 
-gulp.task('sass', function () {
-	return gulp.src('scss/styles.scss')
+function compileSass() {
+	return gulp
+		.src('scss/styles.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./css'))
 		.pipe(livereload());
-});
+}
 
-gulp.task('scss-lint', function() {
-	return gulp.src('scss/styles.scss')
-		.pipe(scsslint());
-});
+function sassLint() {
+	return gulp.src('scss/styles.scss').pipe(scsslint());
+}
 
-gulp.task('sass:watch', function () {
+function watchSass() {
 	livereload.listen();
-	gulp.watch(['scss/styles.scss'], ['sass', 'scss-lint']);
-	gulp.watch('index.html', livereload.reload);
-});
+	gulp.watch(['scss/styles.scss'], gulp.series(compileSass, sassLint));
+	gulp.watch(['index.html']).on('change', function() {
+		livereload.reload();
+	});
+}
+
+gulp.task('sass', gulp.series(compileSass));
+gulp.task('scss:lint', gulp.series(sassLint));
+gulp.task('default', gulp.series(watchSass));
